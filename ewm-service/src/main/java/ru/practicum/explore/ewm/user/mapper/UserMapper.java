9be2241c5c738c76c5379d9f97e52b54acfb30c2
jlove <jcodephilosophy@gmail.com>
2022-12-03 21:00:00 +0300

    какая-то попытка

--- ewm-service/src/main/java/ru/practicum/explore/ewm/user/mapper/UserMapper.java
+++ ewm-service/src/main/java/ru/practicum/explore/ewm/user/mapper/UserMapper.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
     UserEntity toUserEntity(UserDto userDto);
+
     UserDto toUserDto(UserEntity userEntity);
+
     List<UserDto> toUserDtoList(List<UserEntity> users);
 }
