386a262c47f038f2b53fa0d93a1b38e1f427c5d1
jlove <jcodephilosophy@gmail.com>
2022-12-07 22:35:29 +0300

    add: validation on rating

--- ewm-service/src/main/java/ru/practicum/explore/ewm/rating/service/RatingServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/explore/ewm/rating/service/RatingServiceImpl.java
@@ -3,14 +3,19 @@ package ru.practicum.explore.ewm.rating.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.explore.ewm.event.dto.EventRatingDto;
+import ru.practicum.explore.ewm.event.entity.EventEntity;
 import ru.practicum.explore.ewm.event.mapper.EventMapper;
 import ru.practicum.explore.ewm.event.repository.EventRepository;
+import ru.practicum.explore.ewm.exeption.NotFoundException;
+import ru.practicum.explore.ewm.exeption.ValidationException;
 import ru.practicum.explore.ewm.rating.entity.RatingEntity;
 import ru.practicum.explore.ewm.rating.repository.RatingRepository;
 import ru.practicum.explore.ewm.user.dto.UserShortDto;
+import ru.practicum.explore.ewm.user.entity.UserEntity;
 import ru.practicum.explore.ewm.user.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,19 +27,17 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public void like(RatingEntity ratingEntity) {
-        /*
-         * Обязуюсь реализовать все проверки!!!
-         * Просто времени в обрез!!! Функционал рейтинга работает
-         * Молю допустите до диплома!!! :)
-         * */
-//        UserEntity userEntity = userRepository.findById(ratingEntity.getLiked())
-//                .orElseThrow(() -> new NotFoundException("can't find user" + ratingEntity.getLiked()));
-//        Optional<RatingEntity> ratingEntity1 = Optional.ofNullable(ratingRepository.findByLiked(userEntity.getId()));
-//
-//        if (ratingEntity1.get().getLiked().equals(userEntity.getId())
-//                && ratingEntity1.get().getIsLiked().equals(ratingEntity.getIsLiked())) {
-//            throw new ValidationException("u can't liked twice");
-//        }
+        UserEntity userEntity = userRepository.findById(ratingEntity.getLiked())
+                .orElseThrow(() -> new NotFoundException("can't find user" + ratingEntity.getLiked()));
+        EventEntity eventEntity = eventRepository.findById(ratingEntity.getEventId())
+                .orElseThrow(() -> new NotFoundException("can't find event" + ratingEntity.getEventId()));
+        Optional<RatingEntity> ratingEntity1 = Optional.ofNullable(ratingRepository.findByLiked(userEntity.getId()));
+        if (ratingEntity1.isPresent()) {
+            if (ratingEntity1.get().getLiked().equals(userEntity.getId())
+                    && ratingEntity1.get().getIsLiked().equals(ratingEntity.getIsLiked())) {
+                throw new ValidationException("u can't liked twice");
+            }
+        }
         ratingRepository.save(ratingEntity);
     }
 
